7b269af9695d407ee487f4eefd9bb5e3affe8802
HopeAnnihilator <j.nolan.rogers@gmail.com>
2021-12-01 03:21:55 -0600

    i wasnt done

--- day01/solve.py
+++ day01/solve.py
@@ -4,4 +4,4 @@ def day01_01():
 
 def day01_02():
     f = [int(i) for i in open('day01/input', 'r').readlines()]
-    return [f[i] + f[i - 1] + f[i - 2] > f[i - 1] + f[i - 2] + f[i -3] for i in range(1, len(f))].count(True)
+    return [f[i] > f[i -3] for i in range(1, len(f))].count(True)
