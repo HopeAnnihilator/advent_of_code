11285686400663ac29ee1848d25e9155caeb8163
HopeAnnihilator <j.nolan.rogers@gmail.com>
2021-12-01 03:04:16 -0600

    ok fuck me im done

--- day01/solve.py
+++ day01/solve.py
@@ -9,10 +9,10 @@ def day01_01():
 def day01_02():
     total = 0
     f = [int(i) for i in open('day01/input', 'r').readlines()]
+    currWindow = f[0] + f[1] + f[2]
     for i in range(3, len(f)):
-        prevWindow = f[i - 1] + f[i - 2]
-        currWindow = f[i] + prevWindow
-        prevWindow += f[i - 3]
+        prevWindow = currWindow
+        currWindow = f[i] + prevWindow - f[i - 3]
         if (currWindow > prevWindow):
             total += 1
     return total
