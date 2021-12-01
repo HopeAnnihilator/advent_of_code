87493852bcd53ded89735555db033bd9aa5b4eb8
HopeAnnihilator <j.nolan.rogers@gmail.com>
2021-12-01 01:31:11 -0600

    best

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time, os, random
+import time, os, random, getpass
 
 from day01.solve import *
+
+term_delay = 0.5
 
 header = '''
 WELCOME TO THE CONTROL CENTER
@@ -32,7 +34,7 @@ def printer(menu):
             else:
                 print(i.lower(), end = '')
             if i == '\n':
-                time.sleep(0.5)
+                time.sleep(term_delay)
             counter += 1
     else:
         for i in range(len(menu)):
@@ -48,14 +50,14 @@ def help_menu():
     printer(help)
 
 def solve_day(s):
-    time.sleep(0.5)
+    time.sleep(term_delay)
     printer(days[s])
     print('\r')
 
 def command_and_control():
     printer('AWAITING USER INPUT\n\n')
-
-    s = input("")
+    s = input('')
+    time.sleep(term_delay)
     match s.lower().split(' ')[0]:
         case "help":
             help_menu()
@@ -70,10 +72,11 @@ def command_and_control():
                 help_menu()
         case "clear":
             printer('\nPREPARING TO CLEAR\n\n')
-            time.sleep(1)
+            time.sleep(term_delay * 3)
             printer('CLEARING\n\r')
-            time.sleep(0.5)
+            time.sleep(term_delay)
             os.system('cls' if os.name=='nt' else 'clear')
+            time.sleep(0.5)
         case _:
             printer('''\nYOU FUCKED IT UP >:(
                 \rYOU TYPED: ''' + s + '''
