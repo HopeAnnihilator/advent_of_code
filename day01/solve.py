99c788d537e6bb72503cd75cb18255734a72e29f
HopeAnnihilator <j.nolan.rogers@gmail.com>
2021-12-01 02:31:42 -0600

    shorter

--- day01/solve.py
+++ day01/solve.py
@@ -1,39 +1,38 @@
 def day01_01():
-    # empty array
-    depths = []
+    total = 0
 
     # read input
-    with open ('day01/input') as f:
-        data = f.readlines()
+    f = open('day01/input', 'r')
 
-    # parse data
-    for i in data:
-        depths.append(int(i.strip('\n')))
-
-    # find totals for node increases
-    total = 0
-    for i in range(1, len(depths)):
-        if depths[i] > depths[i - 1]:
+    depths = [int(f.readline())]
+    
+    # solve shits
+    for line in zip(f):
+        depths.append(int(line[0]))
+        if depths[0] < depths[1]:
             total += 1
+        depths.pop(0)
     return total
 
 def day01_02():
-    # empty array
     depths = []
+    total = 0
 
     # read input
-    with open ('day01/input') as f:
-        data = f.readlines()
+    f = open('day01/input', 'r')
 
-    # parse data
-    for i in data:
-        depths.append(int(i.strip('\n')))
-    # find total for window increases
-    total = 0
-    for i in range(3, len(depths)):
-        prevWindow = depths[i - 1] + depths [i - 2]
-        currWindow = depths[i] + prevWindow
-        prevWindow += depths[i - 3]
+    # setup 
+    for i in range(3):
+        depths.append(int(f.readline()))
+
+    # solve shit
+    for line in zip(f):
+        depths.append(int(line[0]))
+        prevWindow = depths[1] + depths [2]
+        currWindow = depths[3] + prevWindow
+        prevWindow += depths[0]
         if currWindow > prevWindow:
             total += 1
+        depths.pop(0)
+
     return total
