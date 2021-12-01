a1424511c6940983803ba61973324732570a97fe
HopeAnnihilator <j.nolan.rogers@gmail.com>
2021-12-01 03:32:03 -0600

    i wasnt done

--- day01/solve.py
+++ day01/solve.py
@@ -1,5 +1,7 @@
-def day01_01(f):
+def day01_01():
+    f = [int(i) for i in open('day01/input', 'r').readlines()]
     return [f[i] > f[i - 1] for i in range(1, len(f))].count(True)
 
-def day01_02(f):
+def day01_02():
+    f = [int(i) for i in open('day01/input', 'r').readlines()]
     return [f[i] > f[i -3] for i in range(1, len(f))].count(True)
