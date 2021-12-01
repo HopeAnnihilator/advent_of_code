058c9dd6e5fc3802880e8d5d225bbd6d82157792
HopeAnnihilator <j.nolan.rogers@gmail.com>
2021-12-01 03:20:23 -0600

    i wasnt done

--- day01/solve.py
+++ day01/solve.py
@@ -1,18 +1,7 @@
 def day01_01():
-    total = 0
     f = [int(i) for i in open('day01/input', 'r').readlines()]
-    for i in range(1, len(f)):
-        if (f[i] > f[i - 1]):
-            total += 1
-    return total
+    return [f[i] > f[i - 1] for i in range(1, len(f))].count(True)
 
 def day01_02():
-    total = 0
     f = [int(i) for i in open('day01/input', 'r').readlines()]
-    currWindow = f[0] + f[1] + f[2]
-    for i in range(3, len(f)):
-        prevWindow = currWindow
-        currWindow = f[i] + prevWindow - f[i - 3]
-        if (currWindow > prevWindow):
-            total += 1
-    return total
+    return [f[i] + f[i - 1] + f[i - 2] > f[i - 1] + f[i - 2] + f[i -3] for i in range(1, len(f))].count(True)
