7412043c2c6aeb1444365ddfb6a44bf01e036d40
HopeAnnihilator <j.nolan.rogers@gmail.com>
2021-12-01 02:58:10 -0600

    ok fuck me im done

--- day01/solve.py
+++ day01/solve.py
@@ -1,38 +1,18 @@
 def day01_01():
     total = 0
-
-    # read input
-    f = open('day01/input', 'r')
-
-    depths = [int(f.readline())]
-
-    # solve shits
-    for line in zip(f):
-        depths.append(int(line[0]))
-        if depths[0] < depths[1]:
+    f = [int(i) for i in open('day01/input', 'r').readlines()]
+    for i in range(1, len(f)):
+        if (f[i] > f[i - 1]):
             total += 1
-        depths.pop(0)
     return total
 
 def day01_02():
-    depths = []
     total = 0
-
-    # read input
-    f = open('day01/input', 'r')
-
-    # setup 
-    for i in range(3):
-        depths.append(int(f.readline()))
-
-    # solve shit
-    for line in zip(f):
-        depths.append(int(line[0]))
-        prevWindow = depths[1] + depths[2]
-        currWindow = depths[3] + prevWindow
-        prevWindow += depths[0]
-        if currWindow > prevWindow:
+    f = [int(i) for i in open('day01/input', 'r').readlines()]
+    for i in range(3, len(f)):
+        prevWindow = f[i - 1] + f[i - 2]
+        currWindow = f[i] + prevWindow
+        prevWindow += f[i - 3]
+        if (currWindow > prevWindow):
             total += 1
-        depths.pop(0)
-
     return total
