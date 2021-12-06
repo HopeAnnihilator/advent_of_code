d6b47b348a3eb0e6241de075b3893a1ab49608b9
HopeAnnihilator <j.nolan.rogers@gmail.com>
2021-12-05 21:04:48 -0600

    some cleaning

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # advent_of_code
 
 advent of code with a random programming language chosen to do in addition to standard solution each day!
+
+Uses python 3.10 for some stuff
--- day03/solve.py
+++ day03/solve.py
@@ -17,44 +17,37 @@ def day03():
     }
     return iterator(data)
 
-
 def iterator(data):
     for i in range(1, data['linelen'] - 1):
         for _ in range(data['lines']):
             data['rowIter'].append(data['file'].read(1).decode())
             data['file'].seek(data['linelen'] - 1, 1)
 
-
-        data['rowIterTmp'] = [data['rowIter'][i] for i in data['linesO']]
-        if len(data['rowIterTmp']) == 1:
-            data['oxygenGen'] += data['rowIterTmp'][0]
-        else:
-            data['oxygenGen'] += str(int(data['rowIterTmp'].count('1') >= len(data['rowIterTmp']) / 2))
-    
-            n = 0
-            while n < len(data['linesO']):
-                val = data['linesO'][n]
-                if data['rowIter'][val] != data['oxygenGen'][-1]:
-                    data['linesO'].pop(data['linesO'].index(val))
-                else:
-                    n += 1
-
-        data['rowIterTmp'] = [data['rowIter'][i] for i in data['linesCO']]
-        if len(data['rowIterTmp']) == 1:
-            data['co2Scrubber'] += data['rowIterTmp'][0]
-        else:
-            data['co2Scrubber'] += str(int(data['rowIterTmp'].count('1') < len(data['rowIterTmp']) / 2))
-            n = 0
-            while n < len(data['linesCO']):
-                val = data['linesCO'][n]
-                if data['rowIter'][val] != data['co2Scrubber'][-1]:
-                    data['linesCO'].pop(data['linesCO'].index(val))
-                else:
-                    n += 1
-
+        check_row(data, 'oxygenGen', 'linesO')
+        check_row(data, 'co2Scrubber', 'linesCO')
 
         data['gamma'] += str(int(data['rowIter'].count('1') > data['lines'] / 2))
+
         data['rowIter'] = []
         data['file'].seek(i)
+
     data['epsilon'] = ''.join([str(int(i != '1')) for i in data['gamma']])
     return int(data['gamma'], 2) * int(data['epsilon'], 2), int(data['oxygenGen'], 2) * int(data['co2Scrubber'], 2)
+
+def check_row(data, obj, objValues):
+    data['rowIterTmp'] = [data['rowIter'][i] for i in data[objValues]]
+    if len(data['rowIterTmp']) == 1:
+        data[obj] += data['rowIterTmp'][0]
+    else:
+        if objValues == 'linesO':
+            data[obj] += str(int(data['rowIterTmp'].count('1') > len(data['rowIterTmp']) / 2))
+        else: 
+            data[obj] += str(int(data['rowIterTmp'].count('1') < len(data['rowIterTmp']) / 2))
+
+        n = 0
+        while n < len(data[objValues]):
+            val = data[objValues][n]
+            if data['rowIter'][val] != data[obj][-1]:
+                data[objValues].pop(data[objValues].index(val))
+            else:
+                n += 1
